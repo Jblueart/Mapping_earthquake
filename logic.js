6b4fe9407b0149bc76c8694f80147fbe8a670338
Jblueart <104408782+Jblueart@users.noreply.github.com>
2022-08-07 14:30:41 -0600

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,7 +1,12 @@
 // Add console.log to check to see if our code is working.//
 //console.log("working"); tile layer helps it load faster when it needs to interpret largs bits of data.
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_Key
+});
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_Key
@@ -14,14 +19,16 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
-    Dark: dark
+    "Light" : light,
+    "Dark": dark,
+    "Satellite Streets" : satelliteStreets,
+
   };
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
-    zoom: 2,
-    layers: [dark]
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [satelliteStreets]
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
@@ -35,7 +42,7 @@ d3.json(airportData).then(function(data) {
     L.geoJSON(data, {
         onEachFeature: function(feature, layer) {
         console.log(layer);
-        layer.bindPopup( "<h2>" + feature.properties.city + "</h2>"+"<hr></hr>"+"<h3>" + feature.properties.name + "</h3>");
+        layer.bindPopup( "<h3>" + feature.properties.city + "</h3>"+"<hr></hr>"+"<h4>" + feature.properties.name + "</h4>");
         }
     }).addTo(map);
 });    
@@ -78,3 +85,33 @@ let sanFranAirport =
   //  }
 
   //}).addTo(map);
+
+  //accessing toronto airline routes geojson url
+let torontoData = "https://raw.githubusercontent.com/Jblueart/Mapping_earthquake/main/torontoRoutes.json";
+
+//Create a style for the lines
+let myStyle = {
+    color: "#ffffa1", 
+    weight: 2
+}
+
+//grab the geojson data, create geojson layer with the retrieved data
+//d3.json(torontoData).then(function(data){
+//    console.log(data);
+//    L.geoJSON(data, {
+//        style: myStyle, 
+//        onEachFeature: function (feature, layer) {
+//            layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr> <h3>  Destination: " + feature.properties.dst + "</h3>")
+//        }
+
+//    }).addTo(map);
+//});
+
+
+let torontoHoods = "https://raw.githubusercontent.com/Jblueart/Mapping_earthquake/main/torontoNeighborhoods.json";
+
+//Grab the geoJson data for a new layer
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);    
+        L.geoJSON(data).addTo(map);      
+});
